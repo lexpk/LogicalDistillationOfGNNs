05317cb238406853356199ab433dee9a1dc7e010
lexpk <alexander.pluska@tuwien.ac.at>
2024-05-24 10:06:26 +0200

    uniform evaluation syntax

--- evaluate.sh
+++ evaluate.sh
@@ -1,7 +1,7 @@
-python -m evaluate --dataset AIDS          --devices 4 --kfold 10 --max_steps 1000 --layers 8 --dim 128 --lr 1e-5 --activation ReLU --pooling add  --width 8 --layer_depth 2 --ccp_alpha 1e-3
-python -m evaluate --dataset PROTEINS      --devices 4 --kfold 10 --max_steps 1000 --layers 8 --dim 128 --lr 1e-5 --activation ReLU --pooling mean --width 8 --layer_depth 2 --ccp_alpha 1e-2
-python -m evaluate --dataset BZR           --devices 4 --kfold 10 --max_steps 1000 --layers 8 --dim 128 --lr 1e-5 --activation ReLU --pooling mean --width 8 --layer_depth 2 --ccp_alpha 1e-2
-python -m evaluate --dataset EMLC0         --devices 4 --kfold 10 --max_steps 1000 --layers 8 --dim 128 --lr 1e-5 --activation ReLU --pooling mean --width 8 --layer_depth 2 --ccp_alpha 1e-3
-python -m evaluate --dataset EMLC1         --devices 4 --kfold 10 --max_steps 1000 --layers 8 --dim 128 --lr 1e-5 --activation ReLU --pooling mean --width 8 --layer_depth 2 --ccp_alpha 1e-3
-python -m evaluate --dataset EMLC2         --devices 4 --kfold 10 --max_steps 1000 --layers 8 --dim 128 --lr 1e-4 --activation ReLU --pooling mean --sample_size 1000 --width 16 --layer_depth 2 --ccp_alpha 1e-3
-python -m evaluate --dataset BAMultiShapes --devices 4 --kfold 10 --max_steps 1000 --layers 8 --dim 128 --lr 1e-4 --activation ReLU --pooling mean --width 8 --layer_depth 2 --sample_size 4000 --ccp_alpha 1e-3
+python -m evaluate --dataset AIDS          --devices 4 --kfold 10 --max_steps 1000 --layers 8 --dim 128 --lr 1e-5 --activation ReLU --pooling add  --width 8  --layer_depth 2 --ccp_alpha 1e-3
+python -m evaluate --dataset PROTEINS      --devices 4 --kfold 10 --max_steps 1000 --layers 8 --dim 128 --lr 1e-5 --activation ReLU --pooling mean --width 8  --layer_depth 2 --ccp_alpha 1e-2
+python -m evaluate --dataset BZR           --devices 4 --kfold 10 --max_steps 1000 --layers 8 --dim 128 --lr 1e-5 --activation ReLU --pooling mean --width 8  --layer_depth 2 --ccp_alpha 1e-2
+python -m evaluate --dataset EMLC0         --devices 4 --kfold 10 --max_steps 1000 --layers 8 --dim 128 --lr 1e-5 --activation ReLU --pooling mean --width 8  --layer_depth 2 --ccp_alpha 1e-3
+python -m evaluate --dataset EMLC1         --devices 4 --kfold 10 --max_steps 1000 --layers 8 --dim 128 --lr 1e-5 --activation ReLU --pooling mean --width 8  --layer_depth 2 --ccp_alpha 1e-3
+python -m evaluate --dataset EMLC2         --devices 4 --kfold 10 --max_steps 1000 --layers 8 --dim 128 --lr 1e-4 --activation ReLU --pooling mean --width 16 --layer_depth 2 --sample_size 1000 --ccp_alpha 1e-3
+python -m evaluate --dataset BAMultiShapes --devices 4 --kfold 10 --max_steps 1000 --layers 8 --dim 128 --lr 1e-4 --activation ReLU --pooling mean --width 8  --layer_depth 2 --sample_size 4000 --ccp_alpha 1e-3
